07e1c378de720f5293fcf89c52ca23e206ceb33a
Arvin Asli <88236662+arvinasli@users.noreply.github.com>
2026-01-30 11:30:01 +0330

    Add files via upload

---
+++ E1/7.c
@@ -0,0 +1,9 @@
+// 403105793
+// Arvin Baghal Asl
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello World!");
+    return 0;
+}
---
+++ E1/8.c
@@ -0,0 +1,23 @@
+// 403105793
+// Arvin Baghal Asl
+
+#include <stdio.h>
+
+int main() {
+    printf("       *****       *****\n");
+    printf("     **     **   **     **\n");
+    printf("   **         ** **         **\n");
+    printf(" **            ***            **\n");
+    printf("**                            **\n");
+    printf("**          mavis <3         **\n");
+    printf(" **                         **\n");
+    printf("   **                     **\n");
+    printf("     **                 **\n");
+    printf("       **             **\n");
+    printf("         **         **\n");
+    printf("           **     **\n");
+    printf("             ** **\n");
+    printf("               *");
+    
+    return 0;
+}
